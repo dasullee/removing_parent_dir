6112b57e0af68e7c3b263d50514008fdc9018747
nlee01 <nicoledasullee@gmail.com>
2019-07-17 14:09:29 -0700

    changes image name according to orientation

--- index.py
+++ index.py
@@ -6,18 +6,31 @@ setpath = os.getcwd()
 for root, dirs, files in os.walk(setpath):
     keyWord = "Session_Session 1_Scheduled"
     if keyWord in root:
-        path = root        
-        parent_dir = Path(path).parents[0]        
+        path = root
+        parent_dir = Path(path).parents[0]
         # change path to os.remove
         os.chdir(path)        
         os.remove(files[-1])
         # pop last picture aka hand
         files.pop()
+        length = len(files)
+        # orientation of pictures according to length
+        orderLen6 = ["front","front","angled","angled","side","side",]
+        orderLen8 = ["front","front","front","front","angled","angled","side","side",]
         for f in files:
+            index = files.index(f)
+            if length == 6:               
+                replacement = os.path.splitext(f)[0] + "_" + orderLen6[index] +".jpg"                
+            elif length == 8:
+                replacement = os.path.splitext(f)[0] + "_" + orderLen8[index] +".jpg"                
+            try:
+                # rename picture according to orientation
+                os.rename(f, replacement)
+                f = replacement
+            except:
+                print("We don't know orientation of pictures!")
+            # move picture to parent directory
             shutil.move(path+'\\'+f, parent_dir)
-        try:
-            os.chdir(parent_dir)
-            shutil.rmtree(path)
-            print("success in deletion")
-        except: 
-            print("error in deletion")
+        # change directory and remove previous path
+        os.chdir(parent_dir)
+        shutil.rmtree(path)

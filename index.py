74d448d1584ffc8c7c429c789c7d06ea66f3fde5
nlee01 <nicoledasullee@gmail.com>
2019-07-17 15:48:08 -0700

    Adjusted image title

--- index.py
+++ index.py
@@ -15,22 +15,23 @@ for root, dirs, files in os.walk(setpath):
         files.pop()
         length = len(files)
         # orientation of pictures according to length
-        orderLen6 = ["front","front","angled","angled","side","side",]
-        orderLen8 = ["front","front","front","front","angled","angled","side","side",]
+        orderLen6 = ["front","front","angled","angled","side","side"]
+        orderLen8 = ["front","front","front","front","angled","angled","side","side"]
         for f in files:
             index = files.index(f)
             if length == 6:               
-                replacement = os.path.splitext(f)[0] + "_" + orderLen6[index] +".jpg"                
+                replacement = os.path.basename(parent_dir) + "_" + str(index) + "_" + orderLen6[index] +".jpg"             
             elif length == 8:
-                replacement = os.path.splitext(f)[0] + "_" + orderLen8[index] +".jpg"                
+                replacement = os.path.basename(parent_dir) + "_" + str(index) + "_" + orderLen8[index] +".jpg"          
             try:
                 # rename picture according to orientation
                 os.rename(f, replacement)
                 f = replacement
             except:
-                print("We don't know orientation of pictures!")
+                print("Error!")
             # move picture to parent directory
             shutil.move(path+'\\'+f, parent_dir)
+            
         # change directory and remove previous path
         os.chdir(parent_dir)
         shutil.rmtree(path)
